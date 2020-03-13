c6bee788df880d40996f5858b55653edb1c31f12
Charalampos Gkikas <gkikas@idika.gr>
2020-03-13 10:29:33 +0200

    no main actions

--- autofile/wrappers.py
+++ autofile/wrappers.py
@@ -9,12 +9,3 @@ def easyread_open(file, buffering=-1, errors=None, newline=None, closefd=True, o
     encoding = auto_detect.file_encoding(file)
     with io.open(file, mode='rt', buffering=buffering, encoding=encoding, errors=errors, newline=newline, closefd=closefd, opener=opener) as file_object:
         yield file_object
-
-
-if __name__ == "__main__":
-    import glob
-    for item in glob.iglob(r'tests\*.txt'):
-        print(item)
-        with easyread_open(item) as f:
-            data = f.read()
-            print(data)
